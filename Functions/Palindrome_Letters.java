4cfb551a4bf3576aa2b65b32e4082e13a9639481
Dharshan Reddy <147638482+Dharshanreddy003@users.noreply.github.com>
2025-02-20 15:34:05 +0530

    Add files via upload

---
+++ Functions/Palindrome_Letters.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Palindrome_Letters
+{
+    public static void palindrome(String str)
+    {
+        int n=str.length(); boolean check=false;
+        for(int i=0;i<=n/2;i++)
+        {
+            if(i<=n/2)
+            {
+                if(str.charAt(i)==str.charAt(n-i-1))
+                    check=true;
+                else
+                    check=false;
+            }
+            else 
+                break;
+        }
+        if(check)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+	    String n=sc.next();
+	    palindrome(n);
+	}
+}
