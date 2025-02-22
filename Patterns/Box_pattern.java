7cde8b4aa4fa0227478a5239bbf431636007b138
Dharshan Reddy <147638482+Dharshanreddy003@users.noreply.github.com>
2025-02-22 12:15:28 +0530

    Add files via upload

---
+++ Patterns/Box_pattern.java
@@ -0,0 +1,23 @@
+public class Box_pattern
+{
+ public static void main(String args[])
+ {
+ int n=7;
+ for(int i=1;i<=n;i++)
+ {
+  for(int j=1;j<=n;j++)
+  if(i==1 || j==1 || i==n || j==n)
+  {
+   System.out.print("* ");
+  }
+  else if(j==(n/2)+1 || i==(n/2)+1)
+  {
+   System.out.print("+ ");
+  }
+  else
+   System.out.print("  ");
+  System.out.println();
+  }
+  
+ }
+}
